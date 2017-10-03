e3b28004fb040f54cbb3a59e196bc1ae13ba4b9c
Thongchai T <thongchai.t@dreamhatch.co.th>
2017-10-03 18:53:10 +0700

    change method name

--- Pod/Classes/CollieGalleryDelegate.swift
+++ Pod/Classes/CollieGalleryDelegate.swift
@@ -14,6 +14,6 @@ import UIKit
     /// Called when the gallery index changes
     @objc optional func gallery(_ gallery: CollieGallery, indexChangedTo index: Int)
     
-    @objc optional func onDismiss()
+    @objc optional func onDismissGallery(_ gallery: CollieGallery)
     
 }
